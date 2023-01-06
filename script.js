880dc510cefcc6ba6a6af5e455fb79f0a3342d60
Regina Varybrus <varybrusr@gmail.com>
2023-01-06 18:22:01 +0000

    homework done

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 'use strict';
 
 const buttonEnter = document.getElementById('enter-txt');
-const buttonClear = document.getElementByClassName('clear-button');
-const input = document.getElementById('output-field');
-const textArea = document.getElementByClassName('text');
+const buttonClear = document.getElementById('clear-txt');
+const input = document.getElementById('intput-field');
+const textArea = document.getElementById('text-field');
+const textInput = document.getElementById('hidden-text');
 
-// function enterText () {
-//     console.log('+');
-//     textArea.value = input.value;
-// }
+function enterTextHandler () {   
+    if (input.value != '') {
+        textArea.textContent = input.value; 
+        textInput.classList.remove('visible');
+    } else {
+        textInput.classList.add('visible');
+    }  
+}
+
+function clearTextHandler () {
+    input.value = '';
+    textArea.textContent = '';
+}
 
 
 
-// function clearText () {
-//     console.log('++');
-//     input.value = '';
-// }
-
-buttonEnter.addEventListener('click', () => console.log('Press button'));
-// На странице находится инпут и две кнопки и div.
-// Пользователь может ввести что-то в инпут и нажать на кнопку,
-// после этого в div выше должна отобразится строка с тем что было введено в инпуте.
-// Вторя кнопка очищает инпут и div.
-// Добавте валидацию чтоб с пустым инпутом ничего не добавлялось и под инпутом 
-// писалась надпись: "заполните это поле".
